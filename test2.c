8aee239315073aecff527d96b63ed35debef298b
lixudong <lixudong0325@163.com>
2019-01-18 00:07:12 +0800

    d

--- test2.c
+++ test2.c
@@ -9,25 +9,34 @@ void str_cli(FILE *fp, int sockfd)
 		int maxfd;
 		fd_set rset;
 		FD_ZERO(&rset);
+		
+		int stdineof=0;
 		for(;;)
 		{
+			if(stdineof==0)
 			FD_SET(fileno(fp),&rset);
 			FD_SET(sockfd,&rset);
 			maxfd=max(fileno(fp),sockfd)+1;
 			Select(maxfd,&rset,NULL,NULL,NULL);
 			if(FD_ISSET(sockfd,&rset))
-			{
-				if(Readline(sockfd,recvline,MAXLINE)==0)
+			{				
+				if(Read(sockfd,recvline,MAXLINE)==0)
 				{
+					if(stdineof==1)
+					return;
 					err_quit("str_cli:server terminated prematurely");
 				}
-				Fputs(recvline,stdout);
-		
+				Write(fileno(fp),recvline,MAXLINE);	
 			}
 			if(FD_ISSET(fileno(fp),&rset))
 			{
-				if(Fgets(sendline,MAXLINE,fp)==NULL)
-				return;
+				if(Read(fileno(fp),recvline,MAXLINE)==0)
+				{
+					stdineof=1;
+					FD_CLR(fileno(fp),&rset);
+					Shutdown(sockfd,SHUT_WR);
+					continue;
+				}
 				Writen(sockfd,sendline,strlen(sendline));
 			}
 		}
